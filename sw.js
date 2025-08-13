6c75af0f092602dd91c9a4eb7b66f005b8d46724
iancrotty <iancrotty1976@gmail.com>
2025-08-13 13:43:09 +0100

    Initial PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'sobriety-cache-v1';
+const ASSETS = [
+  './',
+  './drinking-tracker.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match('./drinking-tracker.html')))
+  );
+});
