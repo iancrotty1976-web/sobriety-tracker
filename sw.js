5e44efc2e93b93b46d28b28a6eea532552e00bab
iancrotty <iancrotty1976@gmail.com>
2025-09-03 17:44:15 +0100

    Add offline support via service worker

--- sw.js
+++ sw.js
@@ -1,35 +1,49 @@
-const CACHE = 'sobriety-cache-v1';
-
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE_NAME = "sobriety-tracker-v1";
+const OFFLINE_URLS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
 ];
 
-self.addEventListener('install', event => {
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll(ASSETS).catch(err => {
-        console.error('[SW] Cache addAll failed', err);
-      })
-    )
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(OFFLINE_URLS);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+// Activate
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+// Fetch
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request)
-    )
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html") // Fallback if offline
+        )
+      );
+    })
   );
 });
